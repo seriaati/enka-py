382aed43b5b1c7717f3a02ffe4fc3604d7792866
seriaati <seria.ati@gmail.com>
2024-01-08 22:05:28 +0800

    refactor: Improve asset updater

--- enka/assets/updater.py
+++ enka/assets/updater.py
@@ -1,33 +1,48 @@
 import os
-from typing import TYPE_CHECKING, Final
+from typing import TYPE_CHECKING, Any, Final
 
 import aiofiles
 import orjson
 
-from .file_paths import TEXT_MAP_PATH
+from ..exceptions import AssetUpdateError
+from .file_paths import CHARACTER_DATA_PATH, TEXT_MAP_PATH
 
 if TYPE_CHECKING:
     import aiohttp
 
+__all__ = ("AssetUpdater",)
+
 
 class AssetUpdater:
     def __init__(self, session: "aiohttp.ClientSession") -> None:
-        self.session = session
+        self._session = session
 
         self.TEXT_MAP: Final[
             str
-        ] = "https://raw.githubusercontent.com/EnkaNetwork/API-docs/master/store/loc.json"
+        ] = "https://raw.githubusercontent.com/seriaati/enka-py-assets/main/data/text_map.json"
+        self.CHARACTER_DATA: Final[
+            str
+        ] = "https://raw.githubusercontent.com/EnkaNetwork/API-docs/master/store/characters.json"
 
-    async def fetch_text_map(self) -> dict[str, dict[str, str]]:
-        async with self.session.get(self.TEXT_MAP) as resp:
+    async def _fetch_json(self, url: str) -> Any:
+        async with self._session.get(url) as resp:
+            if resp.status != 200:
+                raise AssetUpdateError(resp.status, url)
+
             return orjson.loads(await resp.read())
 
     async def update(self) -> None:
-        text_map = await self.fetch_text_map()
+        text_map = await self._fetch_json(self.TEXT_MAP)
+        characters = await self._fetch_json(self.CHARACTER_DATA)
 
-        if not os.path.exists(os.path.dirname(TEXT_MAP_PATH)):
-            os.makedirs(os.path.dirname(TEXT_MAP_PATH))
+        data_to_save: dict[str, str] = {
+            TEXT_MAP_PATH: text_map,
+            CHARACTER_DATA_PATH: characters,
+        }
+        for path, data in data_to_save.items():
+            if not os.path.exists(os.path.dirname(path)):
+                os.makedirs(os.path.dirname(path))
 
-        async with aiofiles.open(TEXT_MAP_PATH, "w", encoding="utf-8") as f:
-            bytes_ = orjson.dumps(text_map)
-            await f.write(bytes_.decode())
+            async with aiofiles.open(path, "w", encoding="utf-8") as f:
+                bytes_ = orjson.dumps(data)
+                await f.write(bytes_.decode())
