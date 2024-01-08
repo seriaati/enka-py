ac5f28300a3e1ef5f553615186aad35e4fc2585a
seriaati <seria.ati@gmail.com>
2024-01-08 08:53:26 +0800

    feat: Add async entry points

--- enka/client.py
+++ enka/client.py
@@ -1,12 +1,10 @@
 from enum import StrEnum
-from typing import Any
+from typing import Any, Final
 
 import aiohttp
 
 from .exceptions import raise_for_retcode
-from .models.response import ShowcaseResponse
-
-BASE_URL = "https://enka.network/api/uid/{uid}/"
+from .models.response import GenshinShowcaseResponse
 
 
 class Language(StrEnum):
@@ -28,17 +26,41 @@ class Language(StrEnum):
 
 
 class EnkaNetworkAPI:
-    def __init__(self, language: Language = Language.ENGLISH) -> None:
+    def __init__(
+        self, language: Language = Language.ENGLISH, headers: dict[str, Any] | None = None
+    ) -> None:
         self.language = language
+        self.headers = headers
 
-    @classmethod
-    async def fetch_showcase(cls, uid: str) -> ShowcaseResponse:
-        url = BASE_URL.format(uid=uid)
-        async with aiohttp.ClientSession(
-            headers={"User-Agent": "Enka to GO"}
-        ) as session, session.get(url) as resp:
+        self.GENSHIN_API_URL: Final[str] = "https://enka.network/api/uid/{uid}"
+        self.HSR_API_URL: Final[str] = "https://enka.network/api/hsr/uid/{uid}"
+
+    async def __aenter__(self) -> "EnkaNetworkAPI":
+        await self.start()
+        return self
+
+    async def __aexit__(self, exc_type: Any, exc_val: Any, exc_tb: Any) -> None:
+        await self.close()
+
+    async def _request(self, url: str) -> dict[str, Any]:
+        async with self.session.get(url) as resp:
             if resp.status != 200:
                 raise_for_retcode(resp.status)
 
-            data: dict[str, Any] = await resp.json()
-            return ShowcaseResponse(**data)
+            return await resp.json()
+
+    async def start(self) -> None:
+        self.session = aiohttp.ClientSession(headers=self.headers)
+
+    async def close(self) -> None:
+        await self.session.close()
+
+    async def fetch_genshin_showcase(
+        self, uid: str, *, info_only: bool = False
+    ) -> GenshinShowcaseResponse:
+        url = self.GENSHIN_API_URL.format(uid=uid)
+        if info_only:
+            url += "?info"
+
+        data = await self._request(url)
+        return GenshinShowcaseResponse(**data)
