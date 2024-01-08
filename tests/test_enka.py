afa1c55e2b3026b82326085f3b38eb2bee32b170
seriaati <seria.ati@gmail.com>
2024-01-08 08:56:21 +0800

    test: Use `async with` for API clients

--- tests/test_enka.py
+++ tests/test_enka.py
@@ -5,21 +5,21 @@ from enka.client import EnkaNetworkAPI
 
 @pytest.mark.asyncio
 async def test_fetch_showcase() -> None:
-    api = EnkaNetworkAPI()
-    showcase = await api.fetch_genshin_showcase("901211014")
-    assert showcase.uid == "901211014"
+    async with EnkaNetworkAPI() as api:
+        showcase = await api.fetch_genshin_showcase("901211014")
+        assert showcase.uid == "901211014"
 
 
 @pytest.mark.asyncio
 async def test_empty_showcase() -> None:
-    api = EnkaNetworkAPI()
-    showcase = await api.fetch_genshin_showcase("123456789")
-    assert showcase.uid == "123456789"
-    assert len(showcase.characters) == 0
+    async with EnkaNetworkAPI() as api:
+        showcase = await api.fetch_genshin_showcase("123456789")
+        assert showcase.uid == "123456789"
+        assert len(showcase.characters) == 0
 
 
 @pytest.mark.asyncio
 async def test_traveler_showcase() -> None:
-    api = EnkaNetworkAPI()
-    showcase = await api.fetch_genshin_showcase("600001919")
-    assert showcase.uid == "600001919"
+    async with EnkaNetworkAPI() as api:
+        showcase = await api.fetch_genshin_showcase("600001919")
+        assert showcase.uid == "600001919"
