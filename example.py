ee1156f96ddd65bb920fa96e013fbe7be0f6bfc7
seria <seria.ati@gmail.com>
2024-02-02 09:37:50 +0800

    docs: Update example

--- example.py
+++ example.py
@@ -17,25 +17,19 @@ async def main() -> None:
         print("Achievements:", response.player.achievements)
         print("Namecard:", response.player.namecard.full)
 
-        for showcase_character in response.player.showcase_characters:
+        for character in response.characters:
             print("\n==================\n")
-            print("Level:", showcase_character.level)
-            if showcase_character.costuime_icon:
-                print("Costume icon:", showcase_character.costuime_icon.gacha)
-
-        # for character in response.characters:
-        #     print("\n==================\n")
-        #     print(character.name)
-        #     print("Level:", character.level)
-        #     print("Element:", character.element.name)
-        #     print("Constellation:", len(character.constellations))
-        #     print("Weapon:", character.weapon.name)
-        #     print("Weapon level:", character.weapon.level)
-        #     print("Weapon refinement:", character.weapon.refinement)
-        #     print("Side icon:", character.icon.side)
-        #     print("Circle icon:", character.icon.circle)
-        #     print("Gacha art:", character.icon.gacha)
-        #     print("Front icon:", character.icon.front)
+            print(character.name)
+            print("Level:", character.level)
+            print("Element:", character.element.name)
+            print("Constellation:", len(character.constellations))
+            print("Weapon:", character.weapon.name)
+            print("Weapon level:", character.weapon.level)
+            print("Weapon refinement:", character.weapon.refinement)
+            print("Side icon:", character.icon.side)
+            print("Circle icon:", character.icon.circle)
+            print("Gacha art:", character.icon.gacha)
+            print("Front icon:", character.icon.front)
 
 
 asyncio.run(main())
