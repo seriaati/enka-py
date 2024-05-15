c7e35fbb16bbd7d6c25a081384220cb45a6e4b0a
seria <seria.ati@gmail.com>
2024-05-15 10:25:36 +0900

    Add owner support

--- enka/models/hsr/response.py
+++ enka/models/hsr/response.py
@@ -2,6 +2,8 @@ from typing import Any, List
 
 from pydantic import BaseModel, Field, model_validator
 
+from ..enka.owner import Owner
+
 from .character import Character
 from .player import Player
 
@@ -17,12 +19,14 @@ class ShowcaseResponse(BaseModel):
         player (Player): The player.
         ttl (int): The time to live of the response.
         uid (str): The UID of the showcase.
+        owner (Owner, optional): The owner of the showcase's account.
     """
 
     characters: List[Character] = Field(alias="avatarDetailList")
     player: Player = Field(alias="detailInfo")
     ttl: int = 0
     uid: str
+    owner: Owner | None = None
 
     @model_validator(mode="before")
     def _flatten_data(cls, values: dict[str, Any]) -> dict[str, Any]:
