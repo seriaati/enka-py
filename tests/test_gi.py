d54452d3efb17bfb72a23f780eded9b3e0780c25
seria <seria.ati@gmail.com>
2024-11-19 19:53:05 +0800

    style: Remove useless pytest asyncio mark

--- tests/test_gi.py
+++ tests/test_gi.py
@@ -1,19 +1,15 @@
 from __future__ import annotations
 
-import pytest
-
 from enka import GenshinClient
 from enka.gi import Language
 
 
-@pytest.mark.asyncio
 async def test_fetch_showcase() -> None:
     async with GenshinClient() as api:
         showcase = await api.fetch_showcase("901211014")
         assert showcase.uid == "901211014"
 
 
-@pytest.mark.asyncio
 async def test_empty_showcase() -> None:
     async with GenshinClient() as api:
         showcase = await api.fetch_showcase("123456789")
@@ -21,48 +17,41 @@ async def test_empty_showcase() -> None:
         assert len(showcase.characters) == 0
 
 
-@pytest.mark.asyncio
 async def test_traveler_showcase() -> None:
     async with GenshinClient() as api:
         showcase = await api.fetch_showcase("600001919")
         assert showcase.uid == "600001919"
 
 
-@pytest.mark.asyncio
 async def test_update_assets() -> None:
     async with GenshinClient() as api:
         await api.update_assets()
 
 
-@pytest.mark.asyncio
 async def test_zero_achievement() -> None:
     async with GenshinClient() as api:
         showcase = await api.fetch_showcase("831335714")
         assert showcase.player.achievements == 0
 
 
-@pytest.mark.asyncio
 async def test_new_profile_picture_format() -> None:
     async with GenshinClient() as api:
         showcase = await api.fetch_showcase("724824926")
         assert showcase.player.profile_picture_icon is not None
 
 
-@pytest.mark.asyncio
 async def test_costume() -> None:
     async with GenshinClient() as api:
         showcase = await api.fetch_showcase("738081787")
         assert showcase.player.showcase_characters[3].costume is not None
 
 
-@pytest.mark.asyncio
 async def test_langs() -> None:
     for lang in Language:
         async with GenshinClient(lang) as api:
             await api.fetch_showcase("901211014")
 
 
-@pytest.mark.asyncio
 async def test_owner_and_builds() -> None:
     async with GenshinClient() as api:
         showcase = await api.fetch_showcase("618285856")
@@ -70,7 +59,6 @@ async def test_owner_and_builds() -> None:
         await api.fetch_builds(showcase.owner)
 
 
-@pytest.mark.asyncio
 async def test_raw_and_parse() -> None:
     async with GenshinClient() as api:
         raw = await api.fetch_showcase("901211014", raw=True)
--- tests/test_hsr.py
+++ tests/test_hsr.py
@@ -1,43 +1,35 @@
 from __future__ import annotations
 
-import pytest
-
 from enka import HSRClient
 from enka.hsr import Language
 
 
-@pytest.mark.asyncio
 async def test_fetch_showcase() -> None:
     async with HSRClient() as api:
         await api.fetch_showcase("809162009")
 
 
-@pytest.mark.asyncio
 async def test_trailblazer_showcase() -> None:
     async with HSRClient() as api:
         await api.fetch_showcase("123456786")
 
 
-@pytest.mark.asyncio
 async def test_update_assets() -> None:
     async with HSRClient() as api:
         await api.update_assets()
 
 
-@pytest.mark.asyncio
 async def test_langs() -> None:
     for lang in Language:
         async with HSRClient(lang) as api:
             await api.fetch_showcase("809162009")
 
 
-@pytest.mark.asyncio
 async def test_low_level_acc() -> None:
     async with HSRClient() as api:
         await api.fetch_showcase("829702635")
 
 
-@pytest.mark.asyncio
 async def test_owner_and_builds() -> None:
     async with HSRClient() as api:
         showcase = await api.fetch_showcase("809162009")
@@ -45,14 +37,12 @@ async def test_owner_and_builds() -> None:
         await api.fetch_builds(showcase.owner)
 
 
-@pytest.mark.asyncio
 async def test_raw_and_parse() -> None:
     async with HSRClient() as api:
         raw = await api.fetch_showcase("809162009", raw=True)
         api.parse_showcase(raw)
 
 
-@pytest.mark.asyncio
 async def test_empty_substat_list() -> None:
     async with HSRClient() as api:
         await api.fetch_showcase("800724088")
