3172d13e7d6670d56855e486b1fa6e19d2b792c1
seria <seria.ati@gmail.com>
2025-05-23 09:07:31 +0900

    feat: Add backup API for HSR (#109)

--- enka/constants/common.py
+++ enka/constants/common.py
@@ -2,8 +2,11 @@ from __future__ import annotations
 
 from typing import Final
 
+CN_UID_PREFIXES: Final[tuple[str, ...]] = ("1", "2", "5")
 DEFAULT_TIMEOUT: Final[int] = 5  # Seconds before API request times out
+
 HSR_API_URL: Final[str] = "https://enka.network/api/hsr/uid/{}"
+HSR_BACKUP_API_URL: Final[str] = "https://api.asterity.net/hsr/uid/{}?parse=enka"
 GI_API_URL: Final[str] = "https://enka.network/api/uid/{}"
 ZZZ_API_URL: Final[str] = "https://enka.network/api/zzz/uid/{}"
 PROFILE_API_URL: Final[str] = "https://enka.network/api/profile/{}/hoyos/{}/builds/"
