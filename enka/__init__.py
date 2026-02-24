3b16f9304c8cbbe20df99f81da1fcdc243d6086b
seria <seria.ati@gmail.com>
2026-02-25 00:02:14 +0900

    feat: Disable logger by default

--- enka/__init__.py
+++ enka/__init__.py
@@ -1,6 +1,10 @@
 from __future__ import annotations
 
+from loguru import logger as _logger
+
 from . import errors, gi, hsr, utils, zzz
 from .clients import GenshinClient, HSRClient, ZZZClient, cache
 from .enums.enum import Game
 from .models.enka import *
+
+_logger.disable("enka")  # noqa: RUF067
